87a5a72988ce8bd4069fb94a218350a295111dc7
Minoar98 <minoarhasan@gmail.com>
2023-11-06 23:50:07 +0600

    counter added

--- closure.js
+++ closure.js
@@ -2,3 +2,26 @@ function myFunction() {
     let a = 4;
     return a * a;
   }
+
+  myFunction()
+
+
+  // Initiate counter
+let counter = 0;
+
+// Function to increment counter
+function add() {
+  counter += 1;
+}
+
+// Call add() 3 times
+add();
+add();
+add();
+
+console.log(counter);
+
+counter ++;
+// The counter should now be 3
+
+console.log(counter);
