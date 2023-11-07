12bb0bc128d9d310588ef2367769feaeb2f21e1d
Minoar98 <minoarhasan@gmail.com>
2023-11-07 23:20:21 +0600

    date added

--- dom.js
+++ dom.js
@@ -1,12 +1,17 @@
-// const p = document.getElementById("demo");
+// // const p = document.getElementById("demo");
 
-// p.innerHTML = "test 0" ; //dom method
+// // p.innerHTML = "test 0" ; //dom method
 
 
-const x = document.forms["frm1"];
-console.dir(x);
-let text = "";
-for (let i = 0; i < x.length ;i++) {
-  text += text + x.elements[i].value + "<br>";
-}
-document.getElementById("demo").innerHTML = text;
+// const x = document.forms["frm1"];
+// console.dir(x);
+// let text = "";
+// for (let i = 0; i < x.length ;i++) {
+//   text += text + x.elements[i].value + "<br>";
+// }
+// document.getElementById("demo").innerHTML = text;
+
+
+const element = document.getElementById('demo');
+
+element.innerHTML = 'Today\'s date is ' + Date();
