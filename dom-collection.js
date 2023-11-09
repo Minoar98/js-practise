68644edf97960627965e666c60330dc45fdfcb2a
Minoar98 <minoarhasan@gmail.com>
2023-11-09 18:08:56 +0600

    nodelisand htmlk collection added

--- dom-collection.js
+++ dom-collection.js
@@ -1,1 +1,9 @@
-console.dir(document.querySelectorAll('.hello'));
+const nodelist = (document.querySelectorAll('.hello'));
+
+const htmlCollection = (document.getElementsByClassName('hello'));
+
+console.dir(nodelist);
+
+console.dir(htmlCollection);
+console.log(htmlCollection[0]);
+console.log(nodelist[0]);
