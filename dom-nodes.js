6d73367221987c9dce68052355eb1614f7a037ee
Minoar98 <minoarhasan@gmail.com>
2023-11-09 17:00:22 +0600

    insebefore

--- dom-nodes.js
+++ dom-nodes.js
@@ -5,6 +5,6 @@ const node = document.createTextNode("Hello world");
 para.appendChild(node);//<p>Hello world </p>
 
 const element = document.getElementById("div1");
-const p1 = document.getElementById("p1");
+const p2 = document.getElementById("p2");
 
-element.insertBefore(para, p1);
+element.insertBefore(para, p2);
