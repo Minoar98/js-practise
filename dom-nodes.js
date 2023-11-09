7a53306bab13fe68fb1e2a89c6d427095af533f4
Minoar98 <minoarhasan@gmail.com>
2023-11-09 16:58:16 +0600

    insertbefore added

--- dom-nodes.js
+++ dom-nodes.js
@@ -5,4 +5,6 @@ const node = document.createTextNode("Hello world");
 para.appendChild(node);//<p>Hello world </p>
 
 const element = document.getElementById("div1");
-element.appendChild(para);
+const p1 = document.getElementById("p1");
+
+element.insertBefore(para, p1);
