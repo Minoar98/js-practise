d737d4e614362a7709293bccdfbe5ade26617ccd
Minoar98 <minoarhasan@gmail.com>
2023-11-09 17:02:43 +0600

    remove

--- dom-nodes.js
+++ dom-nodes.js
@@ -1,10 +1,14 @@
-const para = document.createElement("p");
+// const para = document.createElement("p");
 
-const node = document.createTextNode("Hello world");
+// const node = document.createTextNode("Hello world");
 
-para.appendChild(node);//<p>Hello world </p>
+// para.appendChild(node);//<p>Hello world </p>
 
-const element = document.getElementById("div1");
-const p2 = document.getElementById("p2");
+// const element = document.getElementById("div1");
+// const p2 = document.getElementById("p2");
 
-element.insertBefore(para, p2);
+// element.insertBefore(para, p2);
+
+const p2 = document.getElementById('p2');
+
+p2.remove();
